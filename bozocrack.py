6ae5aa4a6f3ae74b205b435bf609e1d6b47fb01c
Luciano Pacheco <lucianopacheco@bEx-42.(none)>
2011-11-24 10:38:35 +1100

    added support to guess one hash without a file

--- bozocrack.py
+++ bozocrack.py
@@ -4,17 +4,37 @@ import re
 from urllib import FancyURLopener
 import sys
 
+HASH_REGEX = re.compile("([a-fA-F0-9]{32})")
+
 class MyOpener(FancyURLopener):
     version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
 
+def dictionary_attack(h, wordlist):
+    for word in wordlist:
+        m = hashlib.md5()
+        m.update(word)
+
+        if m.hexdigest() == h:
+            return word
+
+    return None
+
+def crack_single_hash(h):
+    myopener = MyOpener()
+    response = myopener.open("http://www.google.com/search?q={hash}".format(hash=h))
+    
+    wordlist = response.read().replace('.',' ').replace(':',' ').replace('?','').split(' ')
+    plaintext = dictionary_attack(h, set(wordlist))
+    
+    return plaintext
+
 class BozoCrack(object):
     def __init__(self, filename, *args, **kwargs):
-        hash_regex = re.compile("([a-fA-F0-9]{32})")
         self.hashes = []
         self.cache = {}
         
         with open(filename, 'r') as f:
-            hashes = [x for line in f if hash_regex.match(line) for x in hash_regex.findall(line.replace('\n',''))]
+            hashes = [x for line in f if HASH_REGEX.match(line) for x in HASH_REGEX.findall(line.replace('\n',''))]
 
         self.hashes = sorted(list(set(hashes)))
 
@@ -28,34 +48,13 @@ class BozoCrack(object):
                 print "{hash}:{plaintext}".format(hash = h, plaintext = self.cache[h])
                 continue
 
-            plaintext = self.crack_single_hash(h)
+            plaintext = crack_single_hash(h)
 
             if plaintext:
                 print "{hash}:{plaintext}".format(hash = h, plaintext=plaintext)
                 self.cache[h] = plaintext
                 self.append_to_cache(h, plaintext)
-                
-                
-
-    def dictionary_attack(self, h, wordlist):
-        for word in wordlist:
-            m = hashlib.md5()
-            m.update(word)
-
-            if m.hexdigest() == h:
-                return word
-
-        return None
             
-    def crack_single_hash(self, h):
-        myopener = MyOpener()
-        response = myopener.open("http://www.google.com/search?q={hash}".format(hash=h))
-        
-        wordlist = response.read().replace('.',' ').replace(':',' ').replace('?','').split(' ')
-        plaintext = self.dictionary_attack(h, set(wordlist))
-        
-        return plaintext
-
     def load_cache(self, filename='cache'):
         with open(filename, 'a+') as c:
             for line in c:
@@ -69,7 +68,19 @@ class BozoCrack(object):
 if __name__ == '__main__':
     
     if len(sys.argv) == 2:
-        BozoCrack(sys.argv[1]).crack()
+        target = sys.argv[1]
+        if HASH_REGEX.match(target):
+            plaintext = crack_single_hash(target)
+
+            if plaintext:
+                print "{hash}:{plaintext}".format(hash=target, plaintext=plaintext)
+        else:
+            BozoCrack(target).crack()
     else:
-        print "Usage example: python bozocrack.py file_with_md5_hashes.txt"
+        print """Usage example: 
+\tpython bozocrack.py file_with_md5_hashes.txt
+OR:
+\tpython bozocrack.py fcf1eed8596699624167416a1e7e122e
+
+"""
     
