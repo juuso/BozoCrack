4c198bcc0b10365061ef0062f5fc3c1fed060383
Henrique Pereira <ikkibr@gmail.com>
2014-04-28 22:15:09 -0300

    PEP8
    PEP8

--- bozocrack.py
+++ bozocrack.py
@@ -7,8 +7,10 @@ from optparse import OptionParser
 
 HASH_REGEX = re.compile("([a-fA-F0-9]{32})")
 
+
 class MyOpener(FancyURLopener):
     version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
+
 
 def dictionary_attack(h, wordlist):
     for word in wordlist:
@@ -20,32 +22,39 @@ def dictionary_attack(h, wordlist):
 
     return None
 
+
 def format_it(hash, plaintext):
     return "{hash}:{plaintext}".format(hash=hash, plaintext=plaintext)
 
+
 def crack_single_hash(h):
     myopener = MyOpener()
-    response = myopener.open("http://www.google.com/search?q={hash}".format(hash=h))
-    
-    wordlist = response.read().replace('.',' ').replace(':',' ').replace('?','').split(' ')
+    response = myopener.open(
+        "http://www.google.com/search?q={hash}".format(hash=h))
+
+    wordlist = response.read().replace('.', ' ').replace(
+        ':', ' ').replace('?', '').split(' ')
     plaintext = dictionary_attack(h, set(wordlist))
-    
+
     return plaintext
 
+
 class BozoCrack(object):
+
     def __init__(self, filename, *args, **kwargs):
         self.hashes = []
         self.cache = {}
-        
+
         with open(filename, 'r') as f:
-            hashes = [x.lower() for line in f if HASH_REGEX.match(line) for x in HASH_REGEX.findall(line.replace('\n',''))]
+            hashes = [x.lower() for line in f if HASH_REGEX.match(line)
+                      for x in HASH_REGEX.findall(line.replace('\n', ''))]
 
         self.hashes = sorted(list(set(hashes)))
 
         print "Loaded {count} unique hashes".format(count=len(self.hashes))
 
         self.load_cache()
-                                                     
+
     def crack(self):
         for h in self.hashes:
             if h in self.cache:
@@ -58,11 +67,11 @@ class BozoCrack(object):
                 print format_it(h, plaintext)
                 self.cache[h] = plaintext
                 self.append_to_cache(h, plaintext)
-            
+
     def load_cache(self, filename='cache'):
         with open(filename, 'a+') as c:
             for line in c:
-                line = line.replace('\n','').split(':')
+                line = line.replace('\n', '').split(':')
                 self.cache[line[0]] = line[1]
 
     def append_to_cache(self, h, plaintext, filename='cache'):
@@ -72,11 +81,13 @@ class BozoCrack(object):
 if __name__ == '__main__':
 
     parser = OptionParser()
-    parser.add_option('-s', '--single', metavar='MD5HASH', help='cracks a single hash', dest='single', default=False)
-    parser.add_option('-f', '--file', metavar='HASHFILE', help='cracks multiple hashes on a file', dest='target',)
+    parser.add_option('-s', '--single', metavar='MD5HASH',
+                      help='cracks a single hash', dest='single', default=False)
+    parser.add_option('-f', '--file', metavar='HASHFILE',
+                      help='cracks multiple hashes on a file', dest='target',)
 
     (options, args) = parser.parse_args()
-    
+
     if not options.single and not options.target:
         parser.error("please select -s or -f")
     elif options.single:
