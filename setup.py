301085bf2d1c2b967e27deb33fa807492ca9d76f
Henrique Pereira <ikkibr@gmail.com>
2014-05-01 14:20:39 -0300

    Updated history
    And trying to fix Travis problems

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='pybozocrack',
-    version='1.0',
+    version='1.0.1',
     description='PyBozoCrack is a depressingly effective MD5 password hash cracker with almost zero CPU/GPU load.',
     long_description=readme + '\n\n' + history,
     author='Henrique Pereira',
